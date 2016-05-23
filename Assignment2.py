0d11fcca789e358c08c0317bf6d0ebdbbe5025f4
Victor Lin <vlin@ufl.edu>
2016-05-22 21:33:17 -0400

    Fix memory error, clean up code
    * Memory error was due to changing the global variable HEADER. Fixed now.
    * Use os.path instead of os.chdir
    * Write files in a results directory
    * Removed print_header() and print_row() because we are writing to files now.
    * Add get_significant_rows() to help assignment2()
    * Buff up documentation
    
    Note: ALPHA is somewhat unnecessary when only fetching the top 10 significant rows.

--- Assignment2.py
+++ Assignment2.py
@@ -11,27 +11,23 @@ from goatools import obo_parser
 # Regular expresions for terms relating to cell death
 RE_CELL_DEATH = [re.compile('death', re.I), re.compile('apopto', re.I),
                  re.compile('caspase', re.I)]
-
-# We are filtering out GO "levels" higher than this value (inclusive)
+# We are filtering out GO terms with level higher than this value (inclusive)
 LEVEL = 3
-
 ALPHA = 1e-3
 TOP_RESULTS_CAP = 10
-
-OBO_FILE = 'go-basic.obo'
-
 HEADER = ['GO ID', 'Term', 'Level', 'Depth', 'P-value',
           '# of Genes in Term', '# of Target Genes in Term',
           '# of Total Genes', '# of Target Genes', 'Common Genes']
 
-log = logging.getLogger()
-handler = logging.StreamHandler()
-log.addHandler(handler)
-log.setLevel(logging.DEBUG)
+OBO_FILE = 'go-basic.obo'
+OUTPUT_DIRECTORY = os.path.join(os.path.dirname(os.getcwd()), 'results')
+DMGOS_PATH = os.path.join(os.path.dirname(os.getcwd()), 'DmGOs')
 
 
 def get_rows(soup):
     """
+        Yield rows in a dictionary structure.
+
         Parameters
         ----------
         soup: BeautifulSoup object
@@ -51,40 +47,30 @@ def get_rows(soup):
         yield dict(zip(head, row))
 
 
-def print_header():
-    """Prints a formatted header to match row output"""
-    print '%-21s%-43s%-11s%-20s%-12s%s' % ('Filename', 'Term',
-                                           'P-value', 'GO Tree',
-                                           '# of Genes',
-                                           '# of Target Genes')
-
-
-def print_row(row):
-    """Prints only select columns from the row in a formatted output"""
-    print '%-21s%-43s%-11s%-20s%-12s%s' % (row['Filename'], row['Term'],
-                                           row['P-value'], row['GO Tree'],
-                                           row['# of Genes in Term'],
-                                           row['# of Target Genes in Term'])
-
-
 def get_ancestors(go_obj, i=1):
     """
-        Start recursion with i=1 to get desired diffs
+        Recursively get all ancestors of a GOTerm object.
 
         Returns
         -------
         {(ancestor1, diff), (ancestor2, diff), ...} : set of 2-tuples
+
+        Notes
+        -----
+        Starts recursion with i=1 to get desired diffs
     """
 
     if not go_obj.parents:
         return set()
 
-    return set([(parent, i)
-                for parent in go_obj.parents]) | set.union(*[get_ancestors(parent, i + 1) for parent in go_obj.parents])
+    return set([(parent, i) for parent in go_obj.parents]) | \
+        set.union(*[get_ancestors(parent, i + 1) for parent in go_obj.parents])
 
 
 def generate_parent_levels(rows):
     """
+        Generate parent keys for each row.
+
         Parameters
         ----------
         rows: list of dicts
@@ -93,7 +79,8 @@ def generate_parent_levels(rows):
         Returns
         -------
         int
-            Highest level difference between an ancestor and its term
+            Highest level difference between an ancestor and its term.
+            Used by get_updated_header()
     """
     max_diff = -1
     for row in rows:
@@ -110,13 +97,45 @@ def generate_parent_levels(rows):
 
 def get_updated_header(header, max_diff):
     """Takes in a header and returns an updated list based on max difference"""
-    new_header = header
-    for i in range(1, max_diff + 1):
-        new_header.append('Parents (diff: ' + str(i) + ")")
-    return new_header
+    return header + ['Parents (diff: ' + str(i) + ")"
+                     for i in range(1, max_diff + 1)]
+
+
+def get_significant_rows(soup, p, alpha, term_level_cutoff, parent_level_cutoff):
+    """
+        Yield significant rows based on alpha, with additional keys attached.
+
+        Parameters
+        ----------
+        soup: BeautifulSoup object
+            Contains a parsed 'geneOntology.html' file
+        p: GODag object
+            data from .obo file parsed by obo_parser
+
+        Returns
+        -------
+        ({row1}, {row2}, ...)
+            see get_rows() for row data structure
+    """
+    for row in get_rows(soup):
+        # filter using level, pvalue. some GO IDs are not valid
+        # (ex. 'fly-chr-') so we will ignore those
+        if ('GO:' in row['GO ID'] and
+                p[row['GO ID']].level > term_level_cutoff and
+                row['P-value'] > alpha):
+            term_obj = p[row['GO ID']]
+            row['Level'] = term_obj.level
+            row['Depth'] = term_obj.depth
+            row['Ancestors'] = [(a, d) for (a, d) in get_ancestors(term_obj)
+                                if a.level > parent_level_cutoff]
+            yield row
 
 
 def memory_usage_resource():
+    """
+        Tool for debugging memory usage.
+        Source: http://fa.bianp.net/
+    """
     import resource
     rusage_denom = 1024.
     if sys.platform == 'darwin':
@@ -128,74 +147,55 @@ def memory_usage_resource():
 
 def assignment2():
     """
-        Goes through each folder in the 'GO_folders' directory
+        Goes through each folder in the 'DmGOs' directory (DMGOS_PATH)
         and analyzes the 'geneOntology.html' file.
-        'GO_folders' directory must be a sibling of this file's parent directory
     """
-    log.debug('Memory usage: %s', memory_usage_resource())
     log.info('Parsing %s for level checking', OBO_FILE)
     p = obo_parser.GODag(OBO_FILE)
-    log.debug('Memory usage: %s', memory_usage_resource())
-
-    os.chdir('..')
-    os.chdir('DmGOs')
-    folders = next(os.walk('.'))[1]
 
     start = time.clock()
 
+    folders = next(os.walk(DMGOS_PATH))[1]
     for i, folder in enumerate(folders):
-        log.info('Searching in folder %i of %i: %s',
-                 i + 1, len(folders), folder)
-        log.debug('Memory usage: %s', memory_usage_resource())
-        os.chdir(folder)
-
-        rows = []
-        log.debug('Opening geneOntology.html')
-        with open('geneOntology.html') as go_file:
-            log.debug('Memory usage: %s', memory_usage_resource())
-            log.debug('Parsing geneOntology.html with BeautifulSoup')
+        log.info('Searching in folder %i of %i: %s', i + 1, len(folders), folder)
+        with open(os.path.join(DMGOS_PATH, folder, 'geneOntology.html')) as go_file:
             soup = BeautifulSoup(go_file, "html.parser")
-            time.sleep(1)
-            log.debug('Memory usage: %s', memory_usage_resource())
-            for row in get_rows(soup):
-                # filter using level, pvalue. some GO IDs are not valid
-                # (ex. 'fly-chr-') so we will ignore those
-                if ('GO:' in row['GO ID'] and p[row['GO ID']].level > LEVEL and
-                        row['P-value'] > ALPHA):
-                    GOTerm_obj = p[row['GO ID']]
-                    row['Filename'] = folder
-                    row['Level'] = GOTerm_obj.level
-                    row['Depth'] = GOTerm_obj.depth
-                    row['Ancestors'] = [(a, d) for (a, d) in get_ancestors(GOTerm_obj) if a.level > 2]
-                    rows.append(row)
-            soup = None
-            log.info('Found %i significant terms (p<%f) in %s', len(rows), ALPHA, folder)
-            rows = sorted(rows, key=lambda k: float(k['P-value']))[:TOP_RESULTS_CAP]
-            max_diff = generate_parent_levels(rows)
-            new_header = get_updated_header(HEADER, max_diff)
-            # make sure each row has all keys, regardless if needed.
-            for row in rows:
-                for key in new_header:
-                    if key not in row:
-                        row[key] = None
+            rows = get_significant_rows(soup, p, ALPHA,
+                                        term_level_cutoff=3,
+                                        parent_level_cutoff=2)
+        rows = sorted(list(rows), key=lambda k: float(k['P-value']))[:TOP_RESULTS_CAP]
+        max_diff = generate_parent_levels(rows)
+        new_header = get_updated_header(HEADER, max_diff)
+        # make sure each row has all keys, regardless if needed.
+        for row in rows:
+            for key in new_header:
+                if key not in row:
+                    row[key] = None
 
-            # change directory to parent of DmGOs
-            os.chdir('..')
-            os.chdir('..')
-            with open(folder + '-results.txt', 'w') as out_file:
-                filewriter = csv.writer(out_file, delimiter='\t')
-                log.info('Writing output file for %s (top %i significant terms)', folder, TOP_RESULTS_CAP)
-                filewriter.writerow(new_header)
-                for row in rows:
-                    for col in new_header:
-                        if 'Parents' in col and row[col]:
-                            row[col] = ', '.join(row[col])
-                    filewriter.writerow([row[col] for col in new_header])
-            log.debug('Memory usage: %s', memory_usage_resource())
-            os.chdir('DmGOs')
+        with open(os.path.join(OUTPUT_DIRECTORY, folder + '-results.txt'), 'w') as out_file:
+            filewriter = csv.writer(out_file, delimiter='\t')
+            log.info('Writing output file for %s (top %i significant terms)',
+                     folder, TOP_RESULTS_CAP)
+            filewriter.writerow(new_header)
+            for row in rows:
+                # make parent lists pretty
+                for col in new_header:
+                    if 'Parents' in col and row[col]:
+                        row[col] = ', '.join(row[col])
+                filewriter.writerow([row[col] for col in new_header])
 
     end = time.clock()
-    print 'Run time was %4.2fs' % (end - start)
+    log.info('Run time was %4.2fs', end - start)
+    log.info('Memory usage: %s', memory_usage_resource())
+
 
 if __name__ == "__main__":
+    log = logging.getLogger()
+    handler = logging.StreamHandler()
+    log.addHandler(handler)
+    log.setLevel(logging.INFO)
+
+    if not os.path.exists(OUTPUT_DIRECTORY):
+        os.makedirs(OUTPUT_DIRECTORY)
+
     assignment2()
